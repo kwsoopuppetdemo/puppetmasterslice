0cd713a7eb4f94e4f48f4a9a1bff22e4c81af817
KW <you@example.com>
2017-10-05 01:11:39 +0000

    Test PuppetFile

---
+++ copyFiles.sh
@@ -0,0 +1,2 @@
+cp -Rf /home/centos/git_repo/puppetmasterslice/modules /etc/puppetlabs/code/environments/production/
+cp -Rf /home/centos/git_repo/puppetmasterslice/Puppetfile /etc/puppetlabs/code/environments/production/
